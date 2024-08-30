9b11450daff371f75f16acf94236e0cfced4d227
sneha1722 <kaikurushita@gmail.com>
2024-08-30 11:01:28 +0530

    Cfact.py

---
+++ fact.py
@@ -0,0 +1,10 @@
+num = 4
+factorial = 1
+if num<0:
+ print("factorial doesnt exists")
+elif num==0:
+ print("factorial of 0 is 1")
+else:
+ for i in range(num,num+1):
+   factorial=factorial*1
+print("the factorial",num,"is",factorial)
